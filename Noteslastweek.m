3bcd5a97665dbc48735e820834fc88695c0a0c93
Eckert <eckertal@charite.de>
2018-12-07 13:22:19 +0100

    new file w field map assignment to tasks

---
+++ Noteslastweek.m
@@ -0,0 +1,11 @@
+%% Notes from last week
+% Goals: find the adequate field map for each task
+% This is tricky bc there is no equidistance
+% sometimes it will be a file w/ number 005 for a task which has nr 009
+
+% we have to write a program which identifies the field
+% map folder/ document which is closest to the resp. task!
+
+%% Example: ALCUE
+
+paths(1).ALCUE
