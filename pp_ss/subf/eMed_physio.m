52ff405a56de94b7fb42b6e72a327558b4f51187
Eckert <eckertal@charite.de>
2018-12-14 15:29:35 +0100

    new physio script

---
+++ pp_ss/subf/eMed_physio.m
@@ -0,0 +1,6 @@
+% Insert PhysIO data and use batch editor stuff
+% General outline: 
+% 1) Find PhysIO files
+% 2) find Nifti files
+% 3) get the first DICOM
+% 4) insert batch code and feed it information from nifti header
