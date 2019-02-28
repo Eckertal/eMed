753bd7c3447ac21d602e9e83043e6961393aae6a
mrothkirch <marcus.rothkirch@charite.de>
2019-02-28 17:33:12 +0100

    modification of folder structure (data type nested in tasks)

--- pp_ss/subf/agk_eMed_pp_create_foldstr.m
+++ pp_ss/subf/agk_eMed_pp_create_foldstr.m
@@ -1,33 +1,30 @@
-function agk_eMed_pp_create_foldstr(cur_struct,base_dir_pl)
+function agk_eMed_pp_create_foldstr(cur_struct,base_dir_pl,des_tasks,tasks)
 
-% Create the following folder structure for the data:
+% Create the following folder structure for the data (for the desired tasks):
 %
-%                 |- FACES
-%                 |
-%                 |- ALCUE
-%                 |
-%                 |- N-Back
-%                 |
-%          |- MRI -- MID
-%          |      |
-%          |      |- SST 
-%          |      |
-%          |      |- T1
-%          |      |
-%          |      |- Field maps
+%                    |- MRI
+%          |- FACES -|- Physio 
+%          |         |- log
 %          |
-% Subject --- Physio data
+%          |         |- MRI
+%          |- ALCUE -|- Physio 
+%          |         |- log
 %          |
-%          |            |- Faces
-%          |            |
-%          |            |- ALCUE
-%          |            |
-%          |- Log files -- N-back
-%                       |
-%                       |- MID
-%                       |
-%                       |- SST
-%          
+%          |         |- MRI
+%          |- NBACK -|- Physio 
+%          |         |- log
+%          |   
+%          |       |- MRI
+% Subject --- MID -|- Physio
+%          |       |- log 
+%          |
+%          |       |- MRI
+%          |- SST -|- Physio 
+%          |       |- log
+%          |
+%          |- T1
+%          |
+%          |- Fieldmaps 
 %          
 
 cd(base_dir_pl)
@@ -36,24 +33,17 @@ cur_subf = fullfile(pwd,cur_struct.id);
 % make main subf
 mkdir(cur_subf)
 
-% create folders for MRI, Physio and log files
-mkdir(fullfile(cur_subf,'MRI'));
-mkdir(fullfile(cur_subf,'physio'));
-mkdir(fullfile(cur_subf,'log'));
+% create folders for desired tasks, each with subfolders for MRI data,
+% log files and physio data
+des_tasks_name=cellfun(@(x) tasks(x),num2cell(des_tasks),'UniformOutput',false);
+data_types={'MRI';'log';'physio'};
+des_tasks_name=repmat(des_tasks_name,length(data_types),1);
+data_types=repmat(data_types,1,length(des_tasks));
+cellfun(@(x,y) mkdir(char(fullfile(cur_subf,x,y))),des_tasks_name,data_types);
 
-% create tasks subfolders for MRI data and log files
-all_fields = fieldnames(cur_struct);
-for ff=1:length(all_fields)
-    
-    if ~isempty(regexp(all_fields{ff},'_MRI'))
-        mkdir(fullfile(cur_subf,'MRI',all_fields{ff}(1:...
-            regexp(all_fields{ff},'_MRI')-1)));
-    elseif ~isempty(regexp(all_fields{ff},'_log'))
-        mkdir(fullfile(cur_subf,'log',all_fields{ff}(1:...
-            regexp(all_fields{ff},'_log')-1)));
-        
-    end
-end
+% create a folder for T1 and for fieldmaps
+mkdir(fullfile(cur_subf,'t1'));
+mkdir(fullfile(cur_subf,'fieldmaps'));
 
 cd(base_dir_pl)
 return
